4767b7324857a2c978fbb1e0aed09ae5e0c08243
Juan Camilo C <juancamilovmz@gmail.com>
2020-10-23 06:21:55 -0500

    chore: update api url

--- frontend/src/util/client.js
+++ frontend/src/util/client.js
@@ -4,7 +4,7 @@ const client = axios.create({
 	headers: {
 		"Access-Control-Allow-Origin": "*"
 	},
-	baseURL: "http://104.200.24.96/api/v1"
+	baseURL: "https://jobmatcher.loca.lt/api/v1"
 });
 
 export default client;
