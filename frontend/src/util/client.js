cbf46b39487f26a1cdde523bb79f3f72df07f666
Juan Camilo C <juancamilovmz@gmail.com>
2020-10-23 21:48:28 -0500

    chore: changed api backnd url to heroku deployment

--- frontend/src/util/client.js
+++ frontend/src/util/client.js
@@ -4,7 +4,7 @@ const client = axios.create({
 	headers: {
 		"Access-Control-Allow-Origin": "*"
 	},
-	baseURL: "https://dfe2ffb1f53b.ngrok.io/api/v1"
+	baseURL: "https://api-jobx.pejedev.xyz/api/v1"
 });
 
 export default client;
