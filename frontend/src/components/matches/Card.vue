a8f99d56ba279ca8425e8e084d216b837002d02c
Juan Camilo C <juancc@torre.co>
2020-11-09 17:51:13 -0500

    chore: backend migration

--- frontend/src/components/matches/Card.vue
+++ frontend/src/components/matches/Card.vue
@@ -1,66 +1,46 @@
 <template>
-  <b-card
-    id="Card"
-    class="mx-auto"
-    no-body
-    style="max-width: 25rem"
-    v-bind:img-src="offer.picture"
-    img-alt="Image"
-    img-top
-  >
+<b-card id="Card" class="mx-auto" no-body style="max-width: 20rem" v-bind:img-src="offer.picture" img-alt="Image" img-top>
     <template v-slot:header>
-      <h4 class="mb-0">{{ offer.objective }}</h4>
+        <h4 class="mb-0">{{ offer.objective }}</h4>
     </template>
 
     <b-card-body>
-      <b-card-title>{{ offer.organization }}</b-card-title>
-      <b-card-sub-title class="mb-2">{{ currency }} </b-card-sub-title>
-      <div>
-        <b-badge
-          class="skills"
-          v-for="{ name } in skills"
-          :key="name"
-          pill
-          variant="dark"
-          >{{ name }}</b-badge
-        >
-      </div>
+        <b-card-title>{{ offer.organization }}</b-card-title>
+        <b-card-sub-title class="mb-2">{{ currency }} </b-card-sub-title>
+        <div>
+            <b-badge class="skills" v-for="{ name } in skills" :key="name" pill variant="dark">{{ name }}</b-badge>
+        </div>
     </b-card-body>
 
     <b-card-body id="apply">
-      <a v-bind:href="'https://torre.co/en/jobs/' + offer.torreId"
-        >Apply now...</a
-      >
+        <a v-bind:href="'https://torre.co/en/jobs/' + offer.torreId">Apply now...</a>
     </b-card-body>
 
     <b-card-footer class="text-center">Powered by Torre Api</b-card-footer>
-  </b-card>
+</b-card>
 </template>
 
 <script>
 export default {
-  name: "Card",
-  props: ["offer"],
-  data() {
-    return {
-      currency: `${this.offer.currency} ${this.offer.compensation_min} - ${this.offer.compensation_maxt}`,
-      skills: JSON.parse(this.offer.skills),
-    };
-  },
+    name: "Card",
+    props: ["offer"],
+    data() {
+        return {
+            currency: `${this.offer.currency} ${this.offer.compensation_min} - ${this.offer.compensation_max}`,
+            skills: JSON.parse(this.offer.skills),
+        };
+    },
 };
 </script>
 
 <style>
 #Card {
-  margin-top: 2vh;
-  min-width: 350px;
-  min-height: 70vh;
+    margin-top: 2vh;
+    min-width: 350px;
+    min-height: 70vh;
 }
+
 #apply {
-  text-align: end;
-}
-.skills {
-  margin: 0 3px;
-  height: 1.4rem;
+    text-align: end;
 }
 </style>
--- frontend/src/util/client.js
+++ frontend/src/util/client.js
@@ -4,7 +4,7 @@ const client = axios.create({
 	headers: {
 		"Access-Control-Allow-Origin": "*"
 	},
-	baseURL: "https://api-jobx.pejedev.xyz/api/v1"
+	baseURL: "https://job-matcher-backend.herokuapp.com/api/v1"
 });
 
 export default client;
