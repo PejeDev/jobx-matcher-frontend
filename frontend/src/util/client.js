d44a13de5181872ded0d3d5d8838839db1a65a53
Juan Castañeda <hi@pejedev.xyz>
2021-04-13 01:34:15 -0500

    chore(dependencies): updated backend and forntend dependencies

--- frontend/src/util/client.js
+++ frontend/src/util/client.js
@@ -4,7 +4,7 @@ const client = axios.create({
 	headers: {
 		"Access-Control-Allow-Origin": "*"
 	},
-	baseURL: "https://job-matcher-backend.herokuapp.com/api/v1"
+	baseURL: "https://jobx-matcher.shiaai.xyz/api/v1"
 });
 
 export default client;
